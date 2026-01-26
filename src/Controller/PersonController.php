5c1635f8cb1224f1bd7d2b74f3e3c0db82fa2bb1
Maxence <maxence.coste.mc@gmail.com>
2026-01-26 11:01:53 +0100

    tkt

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -1,31 +1,46 @@
 <?php
 
-// src/Controller/PersonController.php
 namespace App\Controller;
 
 use App\Entity\Person;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
+use Symfony\Component\Routing\Attribute\Route;
 
 class PersonController extends AbstractController
 {
-    #[Route('/person/create', name: 'app_person_create', methods: ['POST'])]
-    public function create(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $data = json_decode($request->getContent(), true);
-
-        $person = new Person();
-        $person->setLastname($data['lastname']);
-        $person->setFirstname($data['firstname']);
-        $person->setEmail($data['email']);
-        $person->setBirthdate(new \DateTime($data['birthdate']));
-
+    #[Route('/person', name: 'app_person_create', methods: ['POST'])]
+    public function create(
+        #[MapRequestPayload] Person $person, 
+        EntityManagerInterface $entityManager
+    ): JsonResponse {
         $entityManager->persist($person);
         $entityManager->flush();
 
-        return new Response('Succès ! Personne créée.', 201);
+        return new JsonResponse(['message' => 'Personne créée avec succès'], Response::HTTP_CREATED);
+    }
+
+    #[Route('/person/{id}', name: 'app_person_get', methods: ['GET'])]
+    public function getPerson(Person $person): JsonResponse
+    {
+        return new JsonResponse([
+            'id' => $person->getId(),
+            'firstname' => $person->getFirstname(),
+            'lastname' => $person->getLastname(),
+            'email' => $person->getEmail(),
+            'birthdate' => $person->getBirthdate()->format('Y-m-d'),
+        ]);
+    }
+
+    // --- ÉTAPE 2.6 : SUPPRIMER UNE PERSONNE ---
+    #[Route('/person/{id}', name: 'app_person_delete', methods: ['DELETE'])]
+    public function delete(Person $person, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($person);
+        $entityManager->flush();
+        return new JsonResponse(['message' => 'Personne supprimée avec succès'], Response::HTTP_OK);
     }
 }
